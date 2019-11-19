e4c6040ef18fe4e7d7b114d0e5f2a78cd8f1c219
Vytautas Krivickas <vytautas1996@gmail.com>
2019-11-19 11:19:36 +0200

    bls_verify

--- helper_functions/src/crypto.rs
+++ helper_functions/src/crypto.rs
@@ -1,8 +1,25 @@
-use bls::{AggregatePublicKey, PublicKey};
+use bls::{
+    AggregatePublicKey, PublicKey, PublicKeyBytes, Signature, SignatureBytes,
+};
+use ssz::DecodeError;
 use ring::digest::{digest, SHA256};
+use types::primitives::Domain;
+use std::convert::TryInto;
 
 pub fn hash(input: &[u8]) -> Vec<u8> {
     digest(&SHA256, input).as_ref().to_vec()
+}
+
+pub fn bls_verify(
+    pubkey: &PublicKeyBytes,
+    message: &[u8],
+    signature: &SignatureBytes,
+    domain: Domain,
+) -> Result<bool, DecodeError> {
+    let public_key: PublicKey = pubkey.try_into()?;
+    let signature: Signature = signature.try_into()?;
+
+    Ok(signature.verify(message, domain, &public_key))
 }
 
 pub fn bls_aggregate_pubkeys(pubkeys: &[PublicKey]) -> AggregatePublicKey {
@@ -16,6 +33,7 @@ pub fn bls_aggregate_pubkeys(pubkeys: &[PublicKey]) -> AggregatePublicKey {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use bls::SecretKey;
 
     #[test]
     fn test_hashing() {
@@ -30,5 +48,29 @@ mod tests {
         ];
 
         assert_eq!(expected_bytes, output_bytes);
+    }
+
+     #[test]
+    fn test_bls_verify_simple() {
+        // Load some keys from a serialized secret key.
+        let secret_key = SecretKey::from_bytes(&[
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x3e, 0x6a, 0x4c, 0x7d, 0xae, 0x8f, 0x35, 0x63, 0xfa, 0xbb, 0x9b, 0x57,
+            0xd0, 0x4b, 0x4b, 0x21, 0xd3, 0xf2, 0xb9, 0xf4, 0x54, 0x4a, 0xdc, 0x7b, 0xed, 0xc6,
+            0xcb, 0xb3, 0x6f, 0x03, 0x6b, 0x10,
+        ]).expect("Byte conversion to secret key failed");
+        let public_key = PublicKey::from_secret_key(&secret_key);
+
+        let msg_string = String::from("test123");
+        let message = msg_string.as_bytes();
+        let domain: Domain = 2;
+        let signature = Signature::new(message, domain, &secret_key);
+
+        let pk_bytes =
+            PublicKeyBytes::from_bytes(public_key.as_bytes().as_slice()).expect("Public key conversion to bytes failed");
+        let sg_bytes =
+            SignatureBytes::from_bytes(signature.as_bytes().as_slice()).expect("Signature key conversion to bytes failed");
+
+        assert_eq!(bls_verify(&pk_bytes, message, &sg_bytes, domain), Ok(true));
     }
 }
