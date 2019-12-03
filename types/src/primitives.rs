91e12c6f00a4f527f01e67cb078d8eabe01ed80b
distlt-team <info@dist.lt>
2019-12-03 19:17:00 +0000

    Update fields in AttestationData

--- types/src/primitives.rs
+++ types/src/primitives.rs
@@ -5,6 +5,7 @@ pub type Epoch = u64;
 pub type Gwei = u64;
 pub type Shard = u64;
 pub type Slot = u64;
+pub type CommitteeIndex = u64;
 pub type ValidatorIndex = u64;
 pub type ValidatorId = PublicKey;
 pub type Version = [u8; 4];
